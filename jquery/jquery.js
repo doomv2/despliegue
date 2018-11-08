997dc6074aedf64362af2f4ab3a761d8cb8a2b44
Raymond Iuhasz <raimond96@gmail.com>
2018-11-08 19:16:54 +0100

    jquery08/11/18

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -4,20 +4,20 @@ $(document).ready(function(){
 
     document.getElementById("id_Env").onclick=function(e){comprobar()}
 
-    function comprobar(){
-           
+    function comprobar(){          
 
         var nom=$('#id_Nom').val();
         var ape=$('#id_Ape').val();   
         if(nom==""){
-        alert("hola");
         
            $("#id_Nom").apend("<span id='nom'>Rellena el campo</span>");
         }
         if(ape==""){
             $("#id_Ape").after("<span id='nom'>Rellena el campo</span>");
         } else{
-            $('#id_Env').apend("Enviado");
+            $('#id_Env').after("Enviado");
+            alert("hola");
+
         }
     }    
 });
---
+++ jquery/jqueryalerta.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+document.getElementById("X").addEventListener("click",cerrar,false);
+
+
+
+})
+function cerrar(){
+
+    $("#alerta").remove();
+  }
